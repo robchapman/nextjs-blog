74dcb0ce3d91a3a93f1d6bf044473e76effa667b
Robert Chapman <robchapman94@gmail.com>
2022-04-25 17:27:57 +0100

    open ai prototype static generation working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import { getSortedPostsData, getGeneratedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
@@ -9,7 +9,7 @@ import { GetStaticProps } from 'next'
 
 export const getStaticProps: GetStaticProps = async() => {
 
-  const allPostsData = getSortedPostsData()
+  const allPostsData = await getGeneratedPostsData()
   return {
     props: {
       allPostsData
