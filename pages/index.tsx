c3d95976159be7bb20f66903cdc1895941eab11f
Robert Chapman <robchapman94@gmail.com>
2022-04-25 14:25:03 +0100

    Add typescript according to lesson guide

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,10 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
-export async function getStaticProps() {
+import { GetStaticProps } from 'next'
+
+export const getStaticProps: GetStaticProps = async() => {
+
   const allPostsData = getSortedPostsData()
   return {
     props: {
@@ -14,7 +17,15 @@ export async function getStaticProps() {
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
